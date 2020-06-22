4bc38c27eb00289fa7fed2fc57925bdfa47a867d
morris-ribs <maumribeiro@gmail.com>
2020-06-22 18:06:24 +0200

    Max temperature in year 1800

---
+++ max_temperatures.py
@@ -0,0 +1,25 @@
+from pyspark import SparkConf, SparkContext
+
+conf = SparkConf().setMaster("local").setAppName("MaxTemperatures")
+sc = SparkContext(conf = conf)
+
+def parseLine(line):
+    fields = line.split(',')
+    stationID = fields[0]
+    entryType = fields[2]
+    temperature = float(fields[3]) * 0.1 # to Farenheit if you want: * (9.0 / 5.0) + 32.0
+    return (stationID, entryType, temperature)
+
+lines = sc.textFile("file:///SparkCourse/1800.csv")
+parsedLines = lines.map(parseLine)
+# filter() removes data from the RDD
+# Here, we want to filter out entries that don't have "TMAX" in the first item of a list of data
+minTemps = parsedLines.filter(lambda x: "TMAX" in x[1])
+# create (stationID, temperature) key/value pairs
+stationTemps = minTemps.map(lambda x: (x[0], x[2]))
+# find minimum temperature by stationID
+minTemps = stationTemps.reduceByKey(lambda x, y: max(x,y))
+results = minTemps.collect()
+
+for result in results:
+    print(result[0] + "\t{:.2f}C".format(result[1]))
