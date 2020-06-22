3b43d366d30ec5f8a5ee928ee606d2d908f2b8be
morris-ribs <maumribeiro@gmail.com>
2020-06-22 18:19:55 +0200

    Word count on a book

---
+++ word_count.py
@@ -0,0 +1,24 @@
+import re
+from pyspark import SparkConf, SparkContext
+
+def normalizeWords(text):
+    return re.compile(r'\W+', re.UNICODE).split(text.lower())
+
+conf = SparkConf().setMaster("local").setAppName("WordCount")
+sc = SparkContext(conf = conf)
+
+input = sc.textFile("file:///sparkcourse/book.txt")
+# flatmap() can create many new elements from each one
+# here we are going to break the text by whitespaces and add all words in a list
+words = input.flatMap(normalizeWords)
+wordCounts = words.map(lambda x: (x, 1)).reduceByKey(lambda x, y: x + y)
+wordCountsSorted = wordCounts.map(lambda x: (x[1], x[0])).sortByKey()
+results = wordCountsSorted.collect()
+
+for result in results:
+    # we convert to ascii so that we do not have issues displaying the results on the terminal
+    # we also ask to ignore any conversion errors
+    count = str(result[0])
+    word = result[1].encode('ascii', 'ignore')
+    if (word):
+        print(word.decode() + ":\t\t" + count)
